5738b20fa4b593f37c3636727d58708636200703
Sin Kim <bye_ok@naver.com>
2022-09-01 18:08:11 +0900

    [220901 - working]

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 1. npm install
 - npm install express socket.io moment
 - npm install -g nodemon
+
+2. YOUTUBE LINK
+- https://www.youtube.com/watch?v=UoKoPP91Qx0&ab_channel=%EB%8D%B0%EB%B8%8C%EB%A6%AC
+- 27:29
--- src/js/chat.js
+++ src/js/chat.js
@@ -2,6 +2,15 @@
 
 const socket = io();
 
+const nickname = document.querySelector('#nickname');
+const chatList = document.querySelector('.chatting-list');
+const chatInput = document.querySelector('.chatting-input');
+const sendButton = document.querySelector('.send-button');
+
+sendButton.addEventListener('click', () => {
+  
+})
+
 socket.emit('chatting', 'from front');
 
 socket.on('chatting', (data) => {
